4c247aed7d002001b24aea5d4f407a5e25d01a3e
Evgeny Kovalenko <gransmastas@gmail.com>
2024-10-31 22:13:59 +0300

    added sorted by price

--- src/components/ProductList/ProductList.tsx
+++ src/components/ProductList/ProductList.tsx
@@ -1,16 +1,41 @@
-import { IProductListProps } from '@/types';
+'use client';
+import { useEffect, useState } from 'react';
 
-import styles from './productList.module.scss'
+import { IProductListProps } from '@/types';
 import ProductCard from '../ProductCard/ProductCard';
 
+import styles from './productList.module.scss'
+
 const ProductList = ({ products }: IProductListProps) => {
+  const [sortedProducts, setSortedProducts] = useState(products);
+  const [isAscending, setIsAscending] = useState(false);
+
+  useEffect(() => {
+    const sortedArray = [...products].sort((a, b) => a.price - b.price);
+    setSortedProducts(sortedArray);
+  }, [products]);
+
+  const handleSortByPrice = () => {
+    const sortedArray = [...sortedProducts].sort((a, b) => {
+      return isAscending ? a.price - b.price : b.price - a.price;
+    });
+    setSortedProducts(sortedArray);
+    setIsAscending(!isAscending);
+  };
+  
   return (
     <div className={styles.content}>
       <div className={styles.breadCrumbs}>Main {'>'} Catalog</div>
       <h1 className={styles.title}>Catalog</h1>
-      <div className={styles.sorted}>Price</div>
+      <div className={styles.sorted} onClick={handleSortByPrice}>
+        Price {isAscending ? (
+          <img src='icons/VectorASC.svg' alt='▼'/>
+        ) : (
+          <img src='icons/VectorDESC.svg' alt='▲'/>
+        )}
+      </div>
       <div className={styles.list}>
-        {products.map(product => (
+        {sortedProducts.map(product => (
           <ProductCard key={product.id} {...product} />
         ))}
       </div>
